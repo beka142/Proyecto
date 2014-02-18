de6ddf7e2306631718b0f89425459c656e21f5e8
beka142 <beka142@hotmail.com>
2014-02-17 19:30:54 -0600

    Inicio del Github
    Tarea

---
+++ juan.php
@@ -0,0 +1,28 @@
+﻿
+<?php 
+//$ruta = $_SERVER['argv'][1];
+
+$ruta = $argv[1];
+
+echo $ruta." \n";
+
+$fp = fopen ( $ruta , "r" ); 
+
+//$fp = fopen ( 'c:\bd.csv' , "r" ); 
+ //var_dump($fp) ;
+
+while (( $data = fgetcsv ( $fp ,1000, ";" )) !== false ) { // Mientras hay líneas que leer...
+	echo "\n ---- \n";
+	$i = 0; 
+	foreach($data as $row) {
+
+		echo "Campo $i: $row\n"; // Muestra todos los campos de la fila actual 
+		$i++ ;
+
+	}
+
+	echo " \n";
+
+} 
+fclose ( $fp ); 
+?>
